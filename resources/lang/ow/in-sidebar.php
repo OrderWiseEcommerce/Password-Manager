79689315fa9e5ba99202f0789dec7d9adf431e55
Terry Moore <terry.moore@orderwise.co.uk>
2023-02-17 14:47:27 +0000

    Initial changes

---
+++ resources/lang/ow/in-sidebar.php
@@ -0,0 +1,10 @@
+<?php return [
+    'apps' => 'Customers',
+    'icon' => 'Icons',
+    'log' => 'Logs',
+    'logout' => 'Logout',
+    'profile' => 'Profile',
+    'tags' => 'Tags',
+    'teams' => 'Teams',
+    'users' => 'Users',
+];
